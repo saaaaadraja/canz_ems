ba81e9208b028d0dc0547b6f2e8a9065519b841f
saaaaadraja <rmsik92@gmail.com>
2021-07-15 16:59:32 +0500

    changes

--- src/App.js
+++ src/App.js
@@ -60,7 +60,7 @@ roleArr.push(data.data.getEmployee.role);
 empSupervisor.push(data.data.getEmployee.supervisor);  
 emp_full_name.push(data.data.getEmployee.full_name);  
 name.push(data.data.getEmployee.employee_name);
-if(data.data.getEmployee.company.toLowerCase()==='canz studios' || data.data.getEmployee.role==='manager hr' || data.data.getEmployee.role==='owner'
+if(data.data.getEmployee.company.toLowerCase()==='canz studios' || data.data.getEmployee.role==='hr manager' || data.data.getEmployee.role==='owner'
 ){
     setFormState({...formState,formType:'signedIn'})
 }

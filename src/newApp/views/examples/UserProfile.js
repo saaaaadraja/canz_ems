1c818a93d1cbcabe5c6567e525809df0a367ca5d
saaaaadraja <rmsik92@gmail.com>
2021-08-03 16:16:52 +0500

    doj

--- src/newApp/views/examples/UserProfile.js
+++ src/newApp/views/examples/UserProfile.js
@@ -79,7 +79,7 @@ const clickHandler=()=>{
           <Col className="order-xl-2 mb-5 mx-5 px-5 mb-xl-0" xl="11"> 
             <Card className="bg-secondary shadow">
               <div className="card-profile-image "  >
-                      <img style={{height:'150px',width:'150px' , objectFit:'fit'}} src={`https://ems3425b0d312534fc887d7f1545129bee9134248-dev.s3.ap-southeast-1.amazonaws.com/public/${employee.employee_pic}`} alt="..."  className="rounded-circle " />
+                      <img style={{height:'150px',width:'150px' , objectFit:'fit'}} src={`https://ems3425b0d312534fc887d7f1545129bee970119-production.s3.eu-west-1.amazonaws.com/public/${employee.employee_pic}`} alt="..."  className="rounded-circle " />
                   </div>
               <CardHeader className="bg-white border-0">
                 
